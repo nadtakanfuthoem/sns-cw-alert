0fd416826e1b88f18b96996f6b2762706c088112
Nadtakan Futhoem <nadtakan.futhoem@tylertech.com>
2025-04-25 18:16:34 -0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sns-cw-alert
+
+credit: https://aws.amazon.com/blogs/mt/get-notified-specific-lambda-function-error-patterns-using-cloudwatch/
---
+++ src/main.py
@@ -0,0 +1,11 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger=logging.getLogger(__name__)
+
+def lambda_handler(event, context):
+    logger.setLevel(logging.DEBUG)
+    logger.debug("This is a sample DEBUG message.. !!")
+    logger.error("This is a sample ERROR message.... !!")
+    logger.info("This is a sample INFO message.. !!")
+    logger.critical("This is a sample 5xx error message.. !!")
